aa5aadb6bdb2d029bc252776c251b8e3ac6a1399
hiroshi-tadokoro-kyushu-u <hiroshi.tadokoro.kyushu.u@gmail.com>
2021-07-22 22:42:04 +0900

    aaa

---
+++ GBCBS_PRT01/Laytime_Count/LC_main.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="./general.css">
+    <script src="./jquery-2.1.3.min.js"></script>
+    <title>INDEX</title>
+</head>
+
+<body>
+a
+
+</body>
+
+
+</html>
---
+++ GBCBS_PRT01/index.php
@@ -0,0 +1,83 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./reset.css">
+    <link rel="stylesheet" href="./general.css">
+    <script src="./jquery-2.1.3.min.js"></script>
+    <title>INDEX</title>
+</head>
+
+<div class="site_header01">
+    <a href="./index.php">
+        <img class="site_logo" src="./logo.png">
+        </img>
+    </a>
+    <nav class="gnav">
+        <ul class="gnav_menu">
+            <li class="gnav_menu_item01"><a href="">LOG-IN</a></li>
+            <li class="gnav_menu_item01"><a href="">User登録</a></li>
+            <li class="gnav_menu_item01"><a href="">XXX</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div class="site_header02">
+    <div></div>
+    <nav class="gnav">
+        <ul class="gnav_menu">
+            <li class="gnav_menu_item02"><a href="./Laytime_Count/LC_main.php">LAYTIME CALCULATION</a></li>
+            <li class="gnav_menu_item02"><a href="">SHIPMENT LOCATION</a></li>
+            <li class="gnav_menu_item02"><a href="">DELIVERY MANAGEMENT</a></li>
+            <li class="gnav_menu_item02"><a href="">VESSEL NOMINATION</a></li>
+        </ul>
+    </nav>
+</div>
+
+<body class="index_background"> 
+    <div class="function_list">
+        <div>
+            <div class="catch_copy">
+                <div class="catch_copy_title">General Bulker Control Basic System</div>
+                <div class="catch_copy_subtitle">(仮称:GBCBS/物流プラットフォーム)</div>
+            </div>
+
+            <div class="user_register">
+                <a href="">
+                    <div class="user_register_text">User登録</div>              
+                </a> 
+            </div>
+        </div>
+            
+        <div class="main_function">
+            <a class="function_picture" href="">
+                <div>LAYTIME CALCULATION</div>
+                <img src="./LAYTIME CALCULATION.JPG" alt="">
+            </a>
+            <a class="function_picture" href="">
+                <div>SHIPMENT LOCATION</div>
+                <img src="./SHIPMENT LOCATION.JPG" alt="">
+            </a>
+            <a class="function_picture" href="">
+                <div>DELIVERY MANAGEMENT</div>
+                <img src="./DELIVERY MANAGEMENT.JPG" alt="">
+            </a>
+            <a class="function_picture" href="">
+                <div>VESSEL NOMINATION</div>
+                <img src="./VESSEL NOMINATION.JPG" alt="">
+            </a>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
